257b28efa2eb2098a4c8ba33dca1abafb3cdc993
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-04 21:16:20 -0300

    Add files via upload

---
+++ exercicioavaliativo07.py
@@ -0,0 +1,8 @@
+#média de 3 números
+n1 = int(input("Informe a nota 1:\n"))
+n2 = int(input("Informe a nota 2:\n"))
+n3 = int(input("Informe a nota 3:\n"))
+
+media = (n1+n2+n3)/3
+
+print(" A média dos 3 números é:\n", media)
